cd105acb59a9cf5b71dba4d060c7d5739e1ac923
Rettherat <kauadesouzafrancisco48@gmail.com>
2025-05-27 17:09:27 -0300

    Add files via upload
    exercicio de verificação de idade 27/05

---
+++ idades.js
@@ -0,0 +1,10 @@
+// ler idade do user
+// se idade for 18 ou mais falar que é maior de idade
+// senao dizer que é menor
+
+let idade = prompt( " digite sua idade " )
+if( idade >= 18 ){
+alert( " voce e maior de idade " )
+}else{
+    alert( " voce e menor de idade " )
+}
